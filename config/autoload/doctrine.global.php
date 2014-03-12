016fe98062b8771247305f371c8d99d9fcb4a299
Tefu Lin <finleyltf@gmail.com>
2014-03-12 12:45:36 +0200

    buffet tab added/ jquery file added to public

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -20,6 +20,7 @@ return array(
                     'host' => '127.0.0.1',
                     'port' => '8889',
                     'dbname' => 'fusion',
+                    'charset' => 'utf8',
                 ),
             ),
         ),
