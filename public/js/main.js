26a06e012711b3a68aecb79be7d8641939474370
Tefu Lin <finleyltf@gmail.com>
2014-03-31 14:15:54 +0300

    buffetAdmin fix, buffet tab init

--- public/js/main.js
+++ public/js/main.js
@@ -605,26 +605,6 @@ $('document').ready(function($) {
 			});
 		});
 
-    $(document).ready(function() {
-        $("#tab-content").find("[id^='tab']").hide(); // Hide all content
-        $("#tabs li:first").attr("id","current"); // Activate the first tab
-        $("#tab-content #tab1").fadeIn(); // Show first tab's content
-
-        $('#tabs a').click(function(e) {
-            e.preventDefault();
-            if ($(this).closest("li").attr("id") == "current"){ //detection for current tab
-                return;
-            }
-            else{
-                $("#tab-content").find("[id^='tab']").hide(); // Hide all content
-                $("#tabs li").attr("id",""); //Reset id's
-                $(this).parent().attr("id","current"); // Activate this
-                $('#' + $(this).attr('name')).fadeIn(); // Show content for the current tab
-            }
-        });
-    });
-
-
 });
 
 //googleMap
